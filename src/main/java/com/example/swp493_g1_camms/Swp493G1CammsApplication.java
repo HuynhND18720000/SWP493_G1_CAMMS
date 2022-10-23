14dd96af1c1407baf1820d93e1bca6ddd7cf8f0d
HuynhND18720000 <HuynhND1872000@gmail.com>
2022-10-23 21:01:37 +0700

    First commit

---
+++ src/main/java/com/example/swp493_g1_camms/Swp493G1CammsApplication.java
@@ -0,0 +1,13 @@
+package com.example.swp493_g1_camms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swp493G1CammsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swp493G1CammsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/swp493_g1_camms/Swp493G1CammsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swp493_g1_camms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swp493G1CammsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
