4526641a9603355fe6e8c02e236a24c231a9f924
minhminhminh29012k@hotmail.com <minhminhminh29012k@hotmail.com>
2022-11-04 03:36:01 +0700

    add warehouse

--- src/main/java/com/example/swp493_g1_camms/services/interfaceService/IWarehouseService.java
+++ src/main/java/com/example/swp493_g1_camms/services/interfaceService/IWarehouseService.java
@@ -1,5 +1,6 @@
 package com.example.swp493_g1_camms.services.interfaceService;
 
+import com.example.swp493_g1_camms.payload.request.ManufacturerDTO;
 import com.example.swp493_g1_camms.payload.request.WarehouseDTO;
 import org.springframework.http.ResponseEntity;
 
@@ -7,5 +8,5 @@ public interface IWarehouseService {
     ResponseEntity<?> findAllWarehouse(int pageIndex, int pageSize) ;
     ResponseEntity<?> findWarehouseById(Long id);
     ResponseEntity<?> editWarehouse(WarehouseDTO warehouseDTO);
-
+    ResponseEntity<?> addWarehouse(WarehouseDTO warehouseDTO);
 }
