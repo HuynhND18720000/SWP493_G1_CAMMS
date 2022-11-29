d88688674b3554c37554a969809738257210cf35
HuynhND18720000 <HuynhND1872000@gmail.com>
2022-11-29 22:02:32 +0700

    upload export order

--- src/main/java/com/example/swp493_g1_camms/entities/StockTakingHistoryDescription.java
+++ src/main/java/com/example/swp493_g1_camms/entities/StockTakingHistoryDescription.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 import java.util.Set;
 
 @Entity
-@Table(name = "stock_taking_history_description")
+@Table(name = "stock_taking_history_des")
 @Getter
 @Setter
 @AllArgsConstructor
